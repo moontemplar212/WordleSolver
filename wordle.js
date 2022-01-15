4e376988d606cc59fbc759dc0344ebcf5c7201de
moontemplar212 <sjdavies24@hotmail.com>
2022-01-15 13:24:27 +1000

    added dictionary list so no API call needed

--- wordle.js
+++ wordle.js
@@ -1,18 +1,26 @@
+const fs = require('fs');
 const prompt = require('prompt-sync')();
-const axios = require('axios');
+// Only want the five letter words
+let dict = require('./dictionary.json').filter(e => e.length === 5);
 
-async function recursiveWordleSolve(input = 0, decision = 0) {
+// Credit to https://raw.githubusercontent.com/davideastmond/wordsolverAPI/master/data/dictionary.json
+
+async function recursiveWordleSolve() {
   input = await getInput();
 
-  console.log(input);
+  console.log(`\n ${input} \n`);
 
   decision = await getResult();
 
-  console.log(decision);
+  console.log(`\n ${decision} \n`);
 
-  const [ sortedInput, sortedDecision ] = await sortResult(input, decision);
+  await filterDict(input, decision);
 
-  recursiveWordleSolve(sortedInput, sortedDecision);
+  if(dict.length === 1) {
+    console.log("You Won!");
+  } else {
+    recursiveWordleSolve();
+  }
 }
 
 async function getInput() {
@@ -33,83 +41,34 @@ async function getResult() {
   return order;
 }
 
-async function sortResult(answer, result) {
-  const a = answer.trim().split('');
-  const r = result.trim().split('');
-  const res = a.map((e, i) => [e, r[i]]);
+async function filterDict(answer, result) {
+  const a = String(answer).trim().split('');
+  const r = String(result).trim().split('');
 
-  console.log(res);
+  console.log('A:', a);
+  console.log('R:', r);
 
-  let exclude = [];
-  let letters = [];
+  r.forEach((v, i) => {
+    if(v === 'c') {
+      // This letter index is correct
+      dict = dict.filter(e => e[i] === a[i]);
+    }
 
-  res.forEach(([l, d]) => {
-    if(d === 'e') {
-      exclude.push(l);
-    } else {
-      letters.push(l);
+    if(v === 'e') {
+      // This letter does not appear at all
+      dict = dict.filter(e => e.search(a[i]) < 0); 
+    }
+
+    if(v === 'i') {
+      dict = dict.filter(e => e.search(a[i]) > 0);
     }
   });
 
-  exclude = [...new Set(exclude)];
-  letters = [...new Set(letters)];
-
-  // unique chars
-  exclude = exclude.join('');
-  letters = letters.join('');
-  
-  // axios request from dword to find those words that are valid given sortedResult
-
-  const wordList = await getWordList(exclude, letters);
-
-  console.log(wordList);
-
-  return [ res, wordList];
+  console.log(dict);
 };
 
-async function getWordList(excludedLetters, letters) {
-  axios({
-    method: 'post',
-    url: 'https://www.dcode.fr/api/',
-    data: {
-      tool: 'words-containing',
-      letters: letters,
-      method: 'letters_order',
-      dico: 'DICO_EN',
-      number_of_letters: '5',
-      exclude_letters: excludedLetters
-    }
-  }).then((response) => {
-    console.log('R:', response);
-    const wordList = response.results;
-    console.log(wordList);
-    return wordList;
-  });
-}
-
 async function main() {
-  // recursiveWordleSolve();
-  // const body = 'tool=words-containing&letters=st&method=letters_order&dico=DICO_EN&number_of_letters=5&exclude_letters=ar';
-  // const response = await axios.post('https://www.dcode.fr/api/', body, {
-  //   headers: {
-  //     'authority': 'www.dcode.fr',
-  //     'method': 'POST',
-  //     'path': '/api/',
-  //     'scheme': 'https',
-  //     'accept': 'application/json, text/javascript, */*; q=0.01',
-  //     'accept-encoding': 'gzip, deflate, br',
-  //     'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-  //     'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-  //     'origin': 'https://www.dcode.fr',
-  //     'referer': 'https://www.dcode.fr/words-containing',
-  //     'sec-fetch-dest': 'empty',
-  //     'sec-fetch-mode': 'cors',
-  //     'sec-fetch-site': 'same-origin',
-  //     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',
-  //     'x-requested-with': 'XMLHttpRequest'
-  //   }
-  // });
-  console.log(response);
+  recursiveWordleSolve();
 }
 
 main();
