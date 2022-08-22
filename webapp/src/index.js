e3ddc8accb6e344cbd464404e84ee9a2d94ed07a
samuel davies <sjdavies24@hotmail.com>
2022-08-22 22:01:45 +1000

    default react app wordle solver

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './webapp/index.css';
-import App from './webapp/App';
-import reportWebVitals from './webapp/reportWebVitals';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
