256cfcf457c411107e7cc38b858eea189cae5d6f
samuel davies <sjdavies24@hotmail.com>
2022-08-23 16:15:37 +1000

    wip

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,12 +1,56 @@
 import './App.css';
 
 const render = () => {
-  return <div className="App">
-    <header className="App-header">
-      Wordle Solver
-    </header>
+  return <div className="app">
+    <div className="main_content_parent">
+      <div className="main_content">
+        <div className="col_side">
+          <div className="nav_parent">
+            <nav className="nav">
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Github</li>
+              </ul>
+            </nav>
+          </div>
+          fkdjalfkhda
+        </div>
+        <div className="col_middle">
+          <header className="app_header">  
+            <div className="page_title">
+              <h1>Wordle Solver</h1>
+              <hr className="line_break"></hr>
+            </div>
+            fkdjalfkhda
+          </header>
+        </div>
+        <div className="col_side">
+          <div className="nav_parent">
+          </div>
+          fkdjalfkhda
+        </div>
+      </div>
+    </div>
   </div>
 }
+
+{/* <header className="app_header">
+      <div className="nav_parent">
+        <nav className="nav">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Github</li>
+          </ul>
+        </nav>
+      </div>
+      <div className="page_title">
+        <h1>Wordle Solver</h1>
+        <hr className="line_break"></hr>
+      </div>
+      <div className="nav_parent"></div>
+    </header> */}
 
 function App() {
   return render();
