ec2ce05b7c70810c910a9270af4644139d567db0
samuel davies <sjdavies24@hotmail.com>
2022-10-01 14:40:19 +1000

    try to work out the Disallowed guesses

--- webapp/src/test.js
+++ webapp/src/test.js
@@ -6,16 +6,19 @@ let N, S, R, I, D, W = 1, G;
  * @param {Int} N: Number of letters
  * @param {Int} R: Number of rows
  */
-const calculateCombinations = (S, N, R) => {
+const calculateCombinations = ({S, N, R} = {}) => {
   P = Math.pow(S, N*R);
-  D = (N > 1) ? Math.pow(N, R) : 0;
+  // D = (N > 1) ? Math.pow(N, R) : 0;
   I = Math.pow(Math.pow(S, N) - (N + 1), R);
   G = I + W;
+  D = P - G;
   
   console.log(`Possible`, P);
   console.log(`Disallowed`, D);
   console.log(`Incorrect`, I);
+  console.log(`Winning`, W);
   console.log(`Guesses`, G);
 }
 
-calculateCombinations(3, 5, 2);
+// Wordle
+calculateCombinations({ S: 3, N: 5, R: 2 });
