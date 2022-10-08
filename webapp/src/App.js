c779213256d820a27d2644f2a2a863e149eb5378
samuel davies <sjdavies24@hotmail.com>
2022-10-08 10:08:03 +1000

    useEffect onKeyDown wip

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,29 +1,7 @@
 import './App.css';
 import { TextInput } from './components/inputs/textInput/textInputComponent';
 import { TextDisplay } from './components/displays/textDisplay/textDisplayComponent';
-
-/**
- * Wordle combination counts
- * 
- * L: Length of words in a row ( number of letters )
- * S: Number of states of each letter
- * R: Number of rows
- * I: Number of incorrect guesses
- * D: Number of disallowed guesses
- * W: Number of winning guesses, always 1
- * G: Total guesses
- * P: All possible guesses
- * 
- */
-
-const L = 5, S = 3, R = 2, W = 1;
-
-let I, D, G, P;
-
-P = Math.pow(S, L*R);
-I = Math.pow(Math.pow(S, L) - (L + 1), R);
-G = I + W;
-D = P - G;
+import { useState, useEffect } from 'react';
 
 const render = () => {
   return <div className="app">
@@ -71,12 +49,12 @@ const render = () => {
           </div>
           <div className='info'>
             {/**
-             * 5 * XXXXI followed by 2nd row all possible = 5 * 237 = 1185
-             * All possible 1st row followed by 5 * XXXXI = 5 * 237 = 1185
-             *                                                      = 2370
-             * 509: 
-             *                                                      ------
-             *                                                      = 2879
+             * 5 * XXXXI followed by 2nd row all possible = 5 * ( 237 + 1 ) = 1190
+             * All possible 1st row followed by 5 * XXXXI = 5 * ( 237 + 1 ) = 1190
+             *                                                              = 2380
+             * 499: 
+             *                                                              ------
+             *                                                              = 2879
              */}
             <TextDisplay name="disallowed">{D}</TextDisplay>
           </div>
@@ -86,7 +64,7 @@ const render = () => {
         </div>
         <div className="col_middle">
           <div className="spacer"></div>
-          <div className="grid_display">
+          <div className="grid_display" onKeyDown={onKeyDownHandler}>
             <div className="grid_row">
               <div className="grid_col">a</div>
               <div className="grid_col">b</div>
@@ -186,6 +164,59 @@ const render = () => {
 }
 
 function App() {
+  /**
+   * Wordle combination counts
+   * 
+   * L: Length of words in a row ( number of letters )
+   * S: Number of states of each letter
+   * R: Number of rows
+   * I: Number of incorrect guesses
+   * D: Number of disallowed guesses
+   * W: Number of winning guesses, always 1
+   * G: Total guesses
+   * P: All possible guesses
+   * 
+   */
+
+  const L = 5, S = 3, R = 2, W = 1;
+
+  let I, D, G, P;
+
+  P = Math.pow(S, L*R);
+  I = Math.pow(Math.pow(S, L) - (L + 1), R);
+  G = I + W;
+  D = P - G;
+
+  const [ displayTextArray, setDisplayTextArray ] = useState(Array.apply(null, L * R));
+
+  /**
+   * I have a display grid with L * R letters => an array L * R long
+   * 
+   * When a keyEvent happens, then I want to push that keyValue to the first position that is null
+   * 
+   * And render the array values into each child in order
+   * 
+   * If a backspace keyEvent is fired then delete the last non null value from the Array
+   * 
+   * 
+   */
+
+  // keyDown
+  const onKeyDownHandler = (e) => {
+    if(e.key === 'Backspace') {
+      setDisplayTextArray( oldArray => [])   
+    }
+    // if(e.key === 'Enter') {
+    //   validateGame();
+    // }
+    const keyValue = e.target.value;
+    setDisplayText(oldValue => [...oldValue, ...newValue ]);
+  }
+
+  useEffect(() => {
+    setDisplayTextArray( oldArray => [ ...oldArray, ...newValue ] );
+  }, oldArray)
+
   return render();
 }
 
